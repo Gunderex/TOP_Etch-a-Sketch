bde3cba77220f65347b7c0d1b68e38ca8f5c43cd
Gunderex <125839591+Gunderex@users.noreply.github.com>
2023-12-06 11:56:49 -0700

    Added unique id's updated add row script to give each row a unique id based on i then use that unique row id to give each square a unique id.

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ function createRow(rowNum) {
     for (let i = 0; i < rowNum; i++) {
         let newRow = document.createElement("div");
         newRow.className = "row";
+        newRow.id = "row" + i;
         canvas.appendChild(newRow);
         
         for (let i = 0; i < rowNum; i++) {
             let newCol = document.createElement("div");
             newCol.className = "col";
+            newCol.id = "col" + i + ":" + newRow.id;
             newRow.appendChild(newCol);
             
         }
@@ -18,3 +20,11 @@ function createRow(rowNum) {
 }
 
 createRow(16);
+
+// const tile = document.querySelectorAll(".col");
+// tile.addEventListener('mouseenter', () => {
+//     tile.classList.add("colFill");
+// })
+// tile.addEventListener('mouseleave', () => {
+//     tile.classList.remove("colFill");
+// })
